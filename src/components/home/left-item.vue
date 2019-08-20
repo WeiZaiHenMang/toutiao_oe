0bc2d1852378c905d01f3211dab2726a0f57e16d
WeiZaiHenMang <774717622@qq.com>
2019-08-20 21:04:21 +0800

    左导航栏样式布局

---
+++ src/components/home/left-item.vue
@@ -0,0 +1,55 @@
+<template>
+  <el-aside width="200px">
+    <div class="dv-img">
+      <img src="../../assets/img/logo_admin.png" width="200" alt />
+    </div>
+    <!-- 导航菜单 -->
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      background-color="#323745"
+      text-color="#adafb5"
+      active-text-color="#ffd04b"
+      :unique-opened="false"
+    >
+      <el-menu-item index="1-1" class="dh-one">首页</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">
+          <span style="padding-left: 20px !important;">内容管理</span>
+        </template>
+        <el-menu-item>发布文章</el-menu-item>
+        <el-menu-item>内容列表</el-menu-item>
+        <el-menu-item>评论列表</el-menu-item>
+      </el-submenu>
+      <el-submenu index="3">
+        <template slot="title">
+          <span style="padding-left: 20px !important">粉丝管理</span>
+        </template>
+        <el-menu-item>图文数据</el-menu-item>
+        <el-menu-item>粉丝概况</el-menu-item>
+        <el-menu-item>粉丝概况</el-menu-item>
+        <el-menu-item>粉丝列表</el-menu-item>
+      </el-submenu>
+      <el-menu-item class="dh-one">账户信息</el-menu-item>
+    </el-menu>
+  </el-aside>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang = 'less'>
+.dv-img {
+  text-align: center;
+  height: 60px;
+  padding-top: 10px;
+  img {
+    height: 34px;
+    width: 80%;
+  }
+}
+.dh-one {
+  padding-left: 40px !important;
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,36 @@
 <template>
-  <div>home</div>
+  <div>
+    <el-container>
+      <el-aside width="200px" class="letf-box">
+        <leftitem></leftitem>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main class="main">Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
-export default {}
+import leftitem from '../../components/home/left-item'
+export default {
+  components: {
+    leftitem: leftitem
+  }
+}
 </script>
 
-<style>
+<style lang ='less'>
+.letf-box {
+  height: 100vh;
+  background-color: #323745;
+}
+
+.el-menu-item {
+  width: 200px;
+}
+.main {
+  background-color: #f2f3f5;
+}
 </style>
